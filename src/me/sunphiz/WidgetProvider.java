59e6439cf7a9fa028286709303c8f97c909cf02f
Joshua Jeong <ys80.jeong@samsung.com>
2013-05-15 00:17:59 +0900

    initial commit

---
+++ src/me/sunphiz/WidgetProvider.java
@@ -0,0 +1,90 @@
+package me.sunphiz;
+
+import me.sunphiz.apdaterviewflipperwidget.R;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+	private static final String TAG = WidgetProvider.class.getSimpleName();
+
+	public static final String REFRESH_ACTION = "me.sunphiz.apdapterviewflipperwidget.REFRESH";
+	public static final String NEXT_ACTION = "me.sunphiz.apdapterviewflipperwidget.NEXT";
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		Log.i(TAG, "onUpdate()");
+
+		for (int id : appWidgetIds) {
+
+			RemoteViews rv = new RemoteViews(context.getPackageName(),
+					R.layout.adapterviewflipper);
+
+			// Specify the service to provide data for the collection widget.
+			// Note that we need to
+			// embed the appWidgetId via the data otherwise it will be ignored.
+			final Intent intent = new Intent(context, WidgetService.class);
+			intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, id);
+			intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+			rv.setRemoteAdapter(R.id.page_flipper, intent);
+
+			// Bind the click intent for the next button on the widget
+			final Intent nextIntent = new Intent(context,
+					WidgetProvider.class);
+			nextIntent.setAction(WidgetProvider.NEXT_ACTION);
+			nextIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, id);
+			final PendingIntent nextPendingIntent = PendingIntent
+					.getBroadcast(context, 0, nextIntent,
+							PendingIntent.FLAG_UPDATE_CURRENT);
+			rv.setOnClickPendingIntent(R.id.next, nextPendingIntent);
+
+			// Bind the click intent for the refresh button on the widget
+			final Intent refreshIntent = new Intent(context,
+					WidgetProvider.class);
+			refreshIntent.setAction(WidgetProvider.REFRESH_ACTION);
+			final PendingIntent refreshPendingIntent = PendingIntent
+					.getBroadcast(context, 0, refreshIntent,
+							PendingIntent.FLAG_UPDATE_CURRENT);
+			rv.setOnClickPendingIntent(R.id.refresh, refreshPendingIntent);
+
+			appWidgetManager.updateAppWidget(id, rv);
+		}
+
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.i(TAG, "onUpdate()");
+
+		final String action = intent.getAction();
+
+		if (action.equals(REFRESH_ACTION)) {
+			final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+			final ComponentName cn = new ComponentName(context,
+					WidgetProvider.class);
+			mgr.notifyAppWidgetViewDataChanged(mgr.getAppWidgetIds(cn),
+					R.id.page_flipper);
+		}
+		if (action.equals(NEXT_ACTION)) {
+			RemoteViews rv = new RemoteViews(context.getPackageName(),
+					R.layout.adapterviewflipper);
+
+			rv.showNext(R.id.page_flipper);
+
+			AppWidgetManager.getInstance(context).partiallyUpdateAppWidget(
+					intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+							AppWidgetManager.INVALID_APPWIDGET_ID), rv);
+		}
+
+		super.onReceive(context, intent);
+	}
+}
---
+++ src/me/sunphiz/WidgetService.java
@@ -0,0 +1,108 @@
+package me.sunphiz;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import me.sunphiz.apdaterviewflipperwidget.R;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+
+public class WidgetService extends RemoteViewsService {
+
+	private static final String TAG = WidgetService.class
+			.getSimpleName();
+
+	final static int[] mLayoutIds = { R.layout.page1, R.layout.page2 };
+
+	@Override
+	public RemoteViewsFactory onGetViewFactory(Intent intent) {
+		Log.d(TAG, "onGetViewFactory()");
+
+		return new ViewFactory(intent);
+	}
+
+	private class ViewFactory implements RemoteViewsService.RemoteViewsFactory {
+		
+		private int mInstanceId = AppWidgetManager.INVALID_APPWIDGET_ID;
+		private Date mUpdateDate = new Date();
+		
+		public ViewFactory(Intent intent) {
+			mInstanceId = intent.getIntExtra(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID );
+		}
+
+		@Override
+		public void onCreate() {
+			Log.i(TAG, "onCreate()");
+
+		}
+
+		@Override
+		public void onDataSetChanged() {
+			Log.i(TAG, "onDataSetChanged()");
+
+			mUpdateDate = new Date();
+		}
+
+		@Override
+		public void onDestroy() {
+			Log.i(TAG, "onDestroy()");
+		}
+
+		@Override
+		public int getCount() {
+			Log.i(TAG, "getCount() " + mLayoutIds.length);
+
+			return mLayoutIds.length;
+		}
+
+		@Override
+		public RemoteViews getViewAt(int position) {
+			Log.i(TAG, "getViewAt()" + position);
+
+			RemoteViews page = new RemoteViews(getPackageName(), mLayoutIds[position]);
+			SimpleDateFormat sdf = (SimpleDateFormat) SimpleDateFormat
+					.getDateTimeInstance();
+			page.setTextViewText(R.id.update_date, sdf.format(mUpdateDate));
+
+			return page;
+		}
+
+		@Override
+		public RemoteViews getLoadingView() {
+			Log.i(TAG, "getLoadingView()");
+
+			return new RemoteViews(getPackageName(), R.layout.loading);
+		}
+
+		@Override
+		public int getViewTypeCount() {
+			Log.i(TAG, "getViewTypeCount()");
+
+			return mLayoutIds.length;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			Log.i(TAG, "getItemId()");
+
+			return position;
+		}
+
+		@Override
+		public boolean hasStableIds() {
+			Log.i(TAG, "hasStableIds()");
+
+			return true;
+		}
+
+	}
+
+}
